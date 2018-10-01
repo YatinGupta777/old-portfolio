185382d09a04af376fb01cd33670e0a453c2a80f
yatin <yatingupta777@yahoo.in>
2018-10-01 17:23:00 +0530

    Github contrib + languages embedded

--- index.js
+++ index.js
@@ -12,3 +12,53 @@ if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && lo
 }
 });
 });
+
+ window.addEventListener('load', async () => { 
+             const GITHUB_USERNAME = 'YatinGupta777'; 
+             const COMMITS_CONTAINER = '#githubCommits'; 
+             const LANGUAGES_CONTAINER = '#githubLanguages'; 
+  
+             const githubStats = await GithubStats(GITHUB_USERNAME); 
+  
+             let githubCommits = document.querySelector(COMMITS_CONTAINER); 
+             /* Render SVG for commit contributions */ 
+             let commitsContribSVG = githubStats.commitsContribSVG({ 
+                 rows: 7, 
+                 space: 4, 
+                 rectWidth: 16, 
+                 levelColors: [ 
+                     { 
+                         minCommits: 0, 
+                         color: '#ebedf0' 
+                     }, 
+                     { 
+                         minCommits: 1, 
+                         color: '#c6e48b' 
+                     }, 
+                     { 
+                         minCommits: 9, 
+                         color: '#7bc96f' 
+                     }, 
+                     { 
+                         minCommits: 17, 
+                         color: '#239a3b' 
+                     }, 
+                     { 
+                         minCommits: 26, 
+                         color: '#196127' 
+                     } 
+                 ] 
+             }); 
+             githubCommits.appendChild(commitsContribSVG); 
+  
+             let githubLanguageDistribution = document.querySelector(LANGUAGES_CONTAINER); 
+             /* Render SVG for language contributions */ 
+             let languageContribSVG = githubStats.languagesContribSVG({ 
+                 barHeight: 20, 
+                 barWidth: githubLanguageDistribution.offsetWidth, 
+                 lineSpacing: 4, 
+                 languageNameWidth: 100, 
+                 fontSize: 14 
+             }); 
+             githubLanguageDistribution.appendChild(languageContribSVG); 
+         }); 
